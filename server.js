a1db1bed3d0158d9b6dfa143222ffcb6823d4c25
Ambit Biswas <ambit.biswas.whjr@gmail.com>
2024-05-22 13:33:58 +0530

    Second Commit for Project 215

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+const { v4: uuidV4 } = require("uuid");
+
+app.get("/", (req, res) => {
+    res.redirect(`/${uuidV4()}`);
+});
+
+app.get("/:room", (req, res) => {
+    res.render("index", { roomId: req.params.room });
+});
 
 server.listen(3030);
